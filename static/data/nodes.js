f848dbb6e40b96d12c05a24c8ccb5547c4c61613
vlad krilovskiy <vlad@krilovskiy.com>
2024-03-31 03:15:03 +0500

    init

---
+++ static/data/nodes.js
@@ -0,0 +1,20 @@
+dataNodes = [
+    {
+        "id": "scope-ru",
+        "type": "scope",
+        "name": "Основной канал",
+        "links": [],
+        "imageUrl": "static/logo/scope-house.png",
+        "url": "https://t.me/russkoe_community"
+    },
+    {
+        "id": "scope-ru-base",
+        "type": "public-group",
+        "name": "РУ: База",
+        "links": [
+            "scope-ru",
+        ],
+        "imageUrl": "static/logo/group-logo.png",
+        "url": "https://t.me/+Wir8KLKNAZllZmEy"
+    }
+];
